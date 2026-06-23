841d2372156b472c914bd3b7ace91dcc4b228a24
sxrxhjxckson <sarah.anne.jackson.24@gmail.com>
2026-06-22 20:11:52 -0400

    Update authRoutes.js
    Added admin sign in

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -1,4 +1,10 @@
 // handles signing in for both types of users
+const express = require("express");
+const db = require("./database");
+const router = express.Router();
+
+router.post("/patron-sign-in", patronSignIn);
+router.post("/admin-sign-in", adminSignIn);
 
 function patronSignIn() {
   // goal: lets regular patron log into their account
@@ -6,8 +12,40 @@ function patronSignIn() {
   // gets email and password from frontend form and checks if matches a record in database
 }
 
-function adminSignIn() {
+async function adminSignIn(req, res) {
   // goal: lets librarians/admins log into backend management side
   // table: USERS (ID, Name, Email, Password, Role [Admin])
   // checks login details against database and confirms if user role is actually an admin
+
+  // Extract email and password
+  const adminEmail = req.body.admin_email;
+  const adminPassword = req.body.admin_password;
+  
+  try {
+    // Database Verification
+    const [userRows] = await db.dbPromise.query(
+      "SELECT * FROM USERS WHERE Email = ?",
+      [adminEmail]
+    );
+    
+    if (userRows.length == 0) {
+      return res.status(401).json({success: false, error: "Invalid email or password."});
+    }
+
+    const user = userRows[0];
+
+    if (user.Role !== 'Admin') {
+      return req.status(403).json({ success: false, error: "Unauthorized access"});
+    }
+
+    if (user.Password !== adminPassword) {
+      return req.status(401).json({ success: false, error: "Invalid email or password."});
+    }
+      // Unlock dashboard
+      res.json({ success: true, message: 'Welcome ${user.Name}'})
+      
+  } catch (error) {
+    console.error("Admin Sign In error: ", error);
+    res.status(500).json({ success: false, error: "Internal server error."});
+  }
 }
